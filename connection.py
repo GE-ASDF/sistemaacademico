50598c2d08b68db3dd80e8dce81d16ff490f7990
Anderson Souza <andersonsouza007@live.com>
2023-02-03 18:47:09 -0300

    Second commit

--- connection.py
+++ connection.py
@@ -6,7 +6,7 @@ class connection:
 
     def __init__(self):
         try:
-            self.db_connection = mysql.connector.connect(host='192.168.1.10', user='prepcurso', password='ke7@ch%', database='preparaadm', port='3307')
+            self.db_connection = mysql.connector.connect(host='192.168.1.30', user='root', password='', database='ouromoderno', port='3306')
             self.db_connection
         except mysql.connector.Error as error:
             if error.errno == errorcode.ER_BAD_DB_ERROR:
